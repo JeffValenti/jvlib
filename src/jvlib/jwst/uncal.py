54ba378538767f97701379a558f9daa1ac7f2dac
Jeff Valenti <valenti@stsci.edu>
2023-03-14 05:14:17 -0400

    add some plot capability to UncalData

--- src/jvlib/jwst/uncal.py
+++ src/jvlib/jwst/uncal.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from astropy.io.fits import open as fits_open
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.figure import Figure
-from numpy import argwhere, diff as np_diff, median, sum as np_sum
+from numpy import argwhere, diff as np_diff, median as np_median, sum as np_sum
 from scipy.ndimage import median_filter
 
 from jvlib.util.obj import use_or_set_default
@@ -14,12 +14,14 @@ from jvlib.util.obj import use_or_set_default
 
 class UncalData:
     def __init__(self, path):
-        self.path = path
-        self.name = self.path.name
+        self.path = Path(path)
         self.hdulist = fits_open(path)
         self.phead = self.hdulist["primary"].header
         self.xhead = self.hdulist["sci"].header
         self.set_metadata()
+        self.maxpanel = 6
+        self.margins = dict(
+            left=0.03, right=0.98, bottom=0.04, top=0.96, hspace=0.06)
 
     def set_metadata(self):
         """Read expoure metadata from FITS primary header."""
@@ -47,13 +49,13 @@ class UncalData:
         if rebase:
             for igroup in range(self.ngroups - 1):
                 image = group_diff[igroup, :, :]
-                group_diff[igroup, :, :] = image - median(image, axis=0)
+                group_diff[igroup, :, :] = image - np_median(image, axis=0)
         return group_diff
 
     def calc_median_group_diff(self, integ, rebase=True):
         """Calculate median of all group difference images in integration."""
         group_diff = self.calc_group_diff(integ, rebase=rebase)
-        return median(group_diff, axis=0)
+        return np_median(group_diff, axis=0)
 
     def set_median_group_diff(self, rebase=True):
         self.median_group_diff = []
@@ -66,6 +68,72 @@ class UncalData:
         self.hdulist.close()
         return
 
+    def init_figure(self, npanel):
+        fig = Figure(figsize=(14, 8))
+        fig.suptitle(f"{self.config},  {self.path.name}")
+        fig.subplots_adjust(**self.margins)
+        axes = fig.subplots(npanel, sharex=True)
+        return fig, axes
+
+    def plot_groups(self, integ, vlim=None, figpath=None):
+        integ_data = self.load_integ_data(integ)
+        npanel = min(self.ngroups, self.maxpanel)
+        npage = ceil(self.ngroups / npanel)
+        if figpath is None:
+            figpath = f"{self.path.stem}_groups_i{integ}.pdf"
+        print(f"writing {figpath}")
+        with PdfPages(figpath) as pdf:
+            for group in range(self.ngroups):
+                image = integ_data[group, :, :]
+                if vlim is None:
+                    medim = np_median(image)
+                    vhw = 5 * np_median(abs(image - medim))
+                    vlim = [medim - vhw, medim + vhw]
+                panel = group % npanel
+                if panel == 0:
+                    fig, axes = self.init_figure(npanel)
+                ax = axes[panel]
+                ax.imshow(
+                    image, origin="lower", aspect="auto", cmap="gray",
+                    vmin=vlim[0], vmax=vlim[1], interpolation="nearest")
+                ax.text(
+                    1.005, 0.5, f"G{group}", va="center",
+                    rotation="vertical", transform=ax.transAxes)
+                if panel == npanel - 1:
+                    pdf.savefig(fig)
+            if panel < npanel - 1:
+                for p in range(panel, npanel):
+                    axes[p].axis("off")
+
+    def plot_group_diffs(self, integ, rebase=True, vlim=None, figpath=None):
+        group_diff = self.calc_group_diff(integ, rebase=rebase)
+        npanel = min(self.ngroups, self.maxpanel)
+        npage = ceil(self.ngroups / npanel)
+        if figpath is None:
+            figpath = f"{self.path.stem}_gdiffs_i{integ}.pdf"
+        print(f"writing {figpath}")
+        with PdfPages(figpath) as pdf:
+            for group in range(self.ngroups - 1):
+                image = group_diff[group, :, :]
+                if vlim is None:
+                    medim = np_median(image)
+                    vhw = 5 * np_median(abs(image - medim))
+                    vlim = [medim - vhw, medim + vhw]
+                panel = group % npanel
+                if panel == 0:
+                    fig, axes = self.init_figure(npanel)
+                ax = axes[panel]
+                ax.imshow(
+                    image, origin="lower", aspect="auto", cmap="gray",
+                    vmin=vlim[0], vmax=vlim[1], interpolation="nearest")
+                ax.text(
+                    1.005, 0.5, f"G{group + 1}-G{group}", va="center",
+                    rotation="vertical", transform=ax.transAxes)
+                if panel == npanel - 1:
+                    pdf.savefig(fig)
+            if panel < npanel - 1:
+                for p in range(panel, npanel):
+                    axes[p].axis("off")
 
 class UncalDataList(list):
     def __init__(self):
